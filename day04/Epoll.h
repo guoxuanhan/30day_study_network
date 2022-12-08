4ec7279e31ab89984124ac17d96d2dffbf36ce69
guoxuanhan <736071985@qq.com>
2022-12-08 17:26:03 +0800

    04-来看看我们的第一个类

---
+++ day04/Epoll.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <sys/epoll.h>
+#include <vector>
+
+class Epoll {
+private:
+    int epfd;
+    epoll_event* events;
+public:
+    Epoll();
+    ~Epoll();
+
+    void addFd(int fd, uint32_t op);
+    std::vector<epoll_event> poll(int timeout = -1);
+};
