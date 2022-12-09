49fbc9eb6c550b090e086d198c3483f26124e8a4
guoxuanhan <736071985@qq.com>
2022-12-09 18:12:04 +0800

    08-一切届时类，连TCP连接也不例外

---
+++ day08/src/Epoll.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <sys/epoll.h>
+#include <vector>
+
+class Channel;
+class Epoll {
+private:
+    int epfd;
+    epoll_event* events;
+public:
+    Epoll();
+    ~Epoll();
+
+    //void addFd(int fd, uint32_t op);
+    //std::vector<epoll_event> poll(int timeout = -1);
+    
+    std::vector<Channel*> poll(int timeout = -1);
+    void updateChannel(Channel*);
+};
