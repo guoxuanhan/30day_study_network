589f22d490557dbfea28f2d6b20753c74ba644f8
guoxuanhan <736071985@qq.com>
2022-12-09 15:45:30 +0800

    06-服务器与事件驱动核心类登场

---
+++ day06/src/InetAddress.cpp
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "InetAddress.h"
+
+
+InetAddress::InetAddress() : addr_len(sizeof(addr)) {
+    bzero(&addr, sizeof(addr));
+}
+
+InetAddress::InetAddress(const char* ip, uint16_t port) {
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
+    addr_len = sizeof(addr);
+}
+
+InetAddress::~InetAddress() {
+
+}
